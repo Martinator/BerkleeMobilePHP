83e8183a4f4db6ed9e7ba59c1a2a138425fc79c3
unknown <mmiddelmann@BOSWKSMMIDDELMA.mia.amadeus.net>
2012-10-12 09:32:23 -0400

    Donate (fund-a-future) screen. (hopefully)

--- demo_mobile_donate.php
+++ demo_mobile_donate.php
@@ -1,34 +1,27 @@
 <?php require_once('_lib/_db.php') ; ?>
-
 <?php require_once('_bidder_info.php') ; ?>
-
-	<html>
+<!DOCTYPE html>
+<html>
 	<head>
-<meta http-equiv="content-type" content="text/html; charset=us-ascii">
-	
-		<title>Berklee Mobile Bidder</title>
-
-		<link rel="stylesheet" type="text/css" href="_mobile.css">	
-	
-	
+		<title>Berklee Gala</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+		<link rel="stylesheet" href="css/themes/BerkleeGala.css" />
+		<link rel="stylesheet" href="berklee-mobile.css" />
+		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
 	</head>
-	<body background="mobile_files/bg-tile-beige.jpg">
-	<center>
+	<body>
 
-	<table border="0" width="100%" cellspacing="0" cellpadding="0" align="center">
-	<tr>
-	<td align="left"><a href="demo_mobile_home.php">Home</a></td>
-	<td align="right"><a href="demo_mobile_donate.php">Refresh</a></td>
-	</tr>
-	</table>
+		<div data-role="page" data-theme="b">
+			<div data-role="header" data-position="fixed">
+				<a hhref="demo_mobile_home.php?docent=<?php echo $docent ; ?>" data-icon="back" rel="external">Home</a>
+				<h1>Fund-A-Future</h1>
 
+			</div><!-- /header -->
 
-	<table border="0" width="480" cellspacing="0" cellpadding="0">
-	<tr>
-	<td><a href="demo_mobile_home.php"><img src="mobile_files/gala-logo.jpg"></a></td>
-	<td><font face="sans-serif" color="#666666" size="4"><b>Bidder<br><br><a href="demo_mobile_home.php"><font face="sans-serif" color="#666666"><?php echo $name ; ?></font></a></b></font></td>
-	</tr>
-	</table>
+			<div data-role="content">
+				
 
 	<?php
 	
@@ -47,20 +40,20 @@
 		//	echo "<meta http-equiv=\"Refresh\" content=\"5;url=demo_mobile_home.php\">" ;
 		//}
 
-		echo "<table border=\"0\" width=\"400\">" ;
+		echo "<ul data-role=\"listview\"  data-theme=\"c\" data-inset=\"true\"  data-divider-theme=\"a\">" ;
 		if ($half_scholarship > 0){
 			$record_donation = mysql_query("INSERT donations (bidder_user_id,half_scholarship,date,time) VALUES ('$bidder_user_id','$half_scholarship','$date','$time')") ;
-			echo "<tr><td><font face=\"sans-serif\" color=\"#666666\">Half scholarships (\$2,500) &nbsp;&nbsp;x&nbsp;&nbsp; </font></td><td><font face=\"sans-serif\" color=\"#666666\">$half_scholarship &nbsp;&nbsp;=&nbsp;&nbsp; </font></td><td align=\"right\"><font face=\"sans-serif\" color=\"#666666\">\$" . number_format($half_scholarship * 2500) . "</font></td></tr>" ;
+			echo "<li>Half scholarships (\$2,500) &times; $half_scholarship = \$" . number_format($half_scholarship * 2500) . "</li>" ;
 		}
 		if ($full_scholarship > 0){
 			$record_donation = mysql_query("INSERT donations (bidder_user_id,full_scholarship,date,time) VALUES ('$bidder_user_id','$full_scholarship','$date','$time')") ;
-			echo "<tr><td><font face=\"sans-serif\" color=\"#666666\">Tuition scholarships (\$5,000) &nbsp;&nbsp;x&nbsp;&nbsp; </font></td><td><font face=\"sans-serif\" color=\"#666666\">$full_scholarship &nbsp;&nbsp;=&nbsp;&nbsp; </font></td><td align=\"right\"><font face=\"sans-serif\" color=\"#666666\">\$" . number_format($full_scholarship * 5000) . "</font></td></tr>" ;
+			echo "<li>Tuition scholarships (\$5,000) &times; $full_scholarship =\$" . number_format($full_scholarship * 5000) . "</li>" ;
 		}
 		if ($other_amount > 0){
 			$record_donation = mysql_query("INSERT donations (bidder_user_id,other_amount,date,time) VALUES ('$bidder_user_id','$other_amount','$date','$time')") ;
-			echo "<tr><td colspan=\"2\"><font face=\"sans-serif\" color=\"#666666\">Other donation: </font></td><td align=\"right\"><font face=\"sans-serif\" color=\"#666666\">\$". number_format($other_amount) . "</font></td></tr>" ;
+			echo "<li>Other donation: <\$". number_format($other_amount) . "</li>" ;
 		}
-		echo "</table>" ;
+		echo "</ul>" ;
 	
 	?>
 
@@ -68,75 +61,69 @@
 	<?php
 		if ($submission != 1){
 	?>
-		<font face="sans-serif" color="#666666"><b>Fund-A-Future</b>: Support these worthy initiatives at Berklee:</font>
-		<br><br>
 		<form action="demo_mobile_donate.php" method="POST">
-		<input type="hidden" name="submission" value="1">
-		<input type="hidden" name="donor_id" value="<?php echo $bidder_user_id ; ?>">
+					<h3>Support these worthy initiatives at Berklee!</h3>
+					
+					<input type="hidden" name="submission" value="1">
+					<input type="hidden" name="donor_id" value="<?php echo $bidder_user_id ; ?>">
 		
-		<table border="0" cellspacing="0" cellpadding="5" width="500">
-		<tr>
-		<td><font face="sans-serif" color="#666666"> Tuition &#43; Room &#38; Board scholarship </font></td>
-		<td>
-			<select name="half_scholarship">
-			<option value="">--</option>
-			<option value="1">1</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-			<option value="4">4</option>
-			<option value="5">5</option>
-			<option value="6">6</option>
-			<option value="7">7</option>
-			<option value="8">8</option>
-			<option value="9">9</option>
-			<option value="10">10</option>
-		</td>
-		<td>
-			<font face="sans-serif" color="#666666">
-			&nbsp;&nbsp; x &nbsp;&nbsp;
-			$8,000 each</font>
-		</td>
-		</tr>
-		<tr>
-		<td><font face="sans-serif" color="#666666"> Tuition-only scholarship </font></td>
-		<td>
-			<select name="full_scholarship">
-			<option value="">--</option>
-			<option value="1">1</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-			<option value="4">4</option>
-			<option value="5">5</option>
-			<option value="6">6</option>
-			<option value="7">7</option>
-			<option value="8">8</option>
-			<option value="9">9</option>
-			<option value="10">10</option>
-		</td>
-		<td>
-			<font face="sans-serif" color="#666666">
-			&nbsp;&nbsp; x &nbsp;&nbsp;
-			$5,000 each</font>
-		</td>
-		</tr>
-		<tr>
-		<td><font face="sans-serif" color="#666666">Other amount</font></td>
-		<td>&nbsp;</td>
-		<td align="right"><font face="sans-serif" color="#666666">$ <input type="number" name="other_amount" size="7"></font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-		</tr>
-		<tr>
-		<td align="center" colspan="3"><input type="submit" value="DONATE!"></td>
-		</tr>
-		</table>
-		<form>
+		
+					<div data-role="fieldcontain">
+						<fieldset data-role="controlgroup">
+							<legend>
+								Scholarships
+							</legend>
+							<label for="half_scholarship">Tuition &#43; Room &#38; Board scholarship ($8,000 each)</label>
+
+							<select name="half_scholarship" id="half_scholarship">
+								<option value="">Tuition &#43; Room &#38; Board</option>
+								<option value="1">1 &times; $8,000</option>
+								<option value="2">2 &times; $8,000</option>
+								<option value="3">3 &times; $8,000</option>
+								<option value="4">4 &times; $8,000</option>
+								<option value="5">5 &times; $8,000</option>
+								<option value="6">6 &times; $8,000</option>
+								<option value="7">7 &times; $8,000</option>
+								<option value="8">8 &times; $8,000</option>
+								<option value="9">9 &times; $8,000</option>
+								<option value="10">10 &times; $8,000</option>
+							</select>
+
+							<label for="full_scholarship">Tuition-only scholarship ($5,000 each)</label>
+
+							<select name="full_scholarship" id="full_scholarship">
+								<option value="">Tuition-only</option>
+								<option value="1">1 &times; $5,000</option>
+								<option value="2">2 &times; $5,000</option>
+								<option value="3">3 &times; $5,000</option>
+								<option value="4">4 &times; $5,000</option>
+								<option value="5">5 &times; $5,000</option>
+								<option value="6">6 &times; $5,000</option>
+								<option value="7">7 &times; $5,000</option>
+								<option value="8">8 &times; $5,000</option>
+								<option value="9">9 &times; $5,000</option>
+								<option value="10">10 &times; $5,000</option>
+							</select>
+						</fieldset>
+					</div>
+
+					<div data-role="fieldcontain" class="ui-hide-label">
+						<label for="other_amount">Other amount</label>
+						<input type="number" name="other_amount" id="other_amount"  placeholder="Other Amount" value=""  />
+					</div>
+					<input type="submit" data-theme="a"  value="DONATE!">
+				</form>
+	
 	<?php
 		} else {
-			echo "<font face=\"sans-serif\" color=\"#666666\"><br><br>Thank you for your generous donations to Fund the Futures of Boston's inner-city youth!</font><br>" ;
+			echo "<h2>Thank you for your generous donations to Fund the Futures of Boston's inner-city youth!</h2>" ;
 			echo "<meta http-equiv=\"Refresh\" content=\"5;url=demo_mobile_home.php\">" ;
 		}
 	?>
 
 
-<?php				
-	echo "</center></body></html>";
+<?php		
+	echo "</div>" ; //close "content"
+	echo "</div>" ; //close "page"	
+	echo "</body></html>";
 ?>
